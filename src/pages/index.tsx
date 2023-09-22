d7eb4e21d83dc66497f3c854ab343a04948f57b2
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-09-22 17:33:58 +0900

    chor: add chakra, recoil

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>study-recoil</title>
+      </Head>
+      <main>
+        hello
+      </main>
+    </div>
+  )
+}
