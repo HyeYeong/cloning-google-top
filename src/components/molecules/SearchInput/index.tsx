040e9a9fa794fba6fdd053b7c4797835f72f265d
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-09-26 11:07:59 +0900

    fix: search inputの中身変更-icon追加

--- src/components/molecules/SearchInput/index.tsx
+++ src/components/molecules/SearchInput/index.tsx
@@ -1,22 +1,33 @@
 import { useRecoilState } from 'recoil';
 import { ChangeEvent } from 'react';
-import { Input, Box, Icon } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { Input, Box, Stack, InputGroup, InputLeftElement } from '@chakra-ui/react'
 
 export const SearchInput = () => {
 
   return (
-    <Box display={'flex'}>
-      <Input
-        borderRadius={'full'}
-        maxWidth={'600px'}
-        fontSize={'16px'}
-        borderWidth={0}
-        bgColor={'#fff'}
-        boxShadow={'rgba(0, 0, 0, 0.1) 0px 10px 20px, rgba(0, 0, 0, 0.1) 0px 3px 3px'}
-        padding={'20px'}
-        placeholder='Search Movie name or anything URL'
-        _placeholder={{ opacity: 1, color: 'gray.700' }}
-      />
-    </Box>
+    <Stack>
+      <InputGroup
+        width={'100%'}
+      >
+        <InputLeftElement
+          pointerEvents='none'
+          padding={'23px 20px 23px 30px'}
+        >
+          <SearchIcon color='gray.800' />
+        </InputLeftElement>
+        <Input
+          type='text'
+          borderRadius={'full'}
+          fontSize={'16px'}
+          borderWidth={0}
+          bgColor={'#fff'}
+          boxShadow={'rgba(0, 0, 0, 0.1) 0px 10px 20px, rgba(0, 0, 0, 0.1) 0px 3px 3px'}
+          padding={'23px 20px 23px 50px'}
+          placeholder='Search Movie name or anything URL'
+          _placeholder={{ opacity: 1, color: 'gray.700' }}
+        />
+      </InputGroup>
+    </Stack>
   )
 };
